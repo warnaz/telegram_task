bf6658613ad8289dbfa515c4331ac64346dc5610
warnaz <124733559+warnaz@users.noreply.github.com>
2024-06-17 17:31:40 +0300

    Update funnel.py

--- src/services/funnel.py
+++ src/services/funnel.py
@@ -4,7 +4,7 @@ import datetime
 from pyrogram.types import Message
 from sqlalchemy import select
 
-from configs.messages import MESSAGES
+from configs.messages import MESSAGES, FunnelMessage
 from src.core.loader import client
 from src.database.database import sessionmaker
 from src.database.models.user import UserModel, UserStatus
@@ -21,30 +21,47 @@ async def run() -> None:
 
             for user in users:
                 if user.status == UserStatus.ALIVE:
-                    await handle_user(user)
+                    status = await handle_user(user)
+                    if status:
+                        user.status = status
+                        async with session.begin():
+                            await session.commit()
 
             await asyncio.sleep(delay)
 
 
-async def handle_user(user: UserModel) -> None:
+async def handle_user(user: UserModel) -> UserStatus | None:
     chat_id = user.username or user.id
     now = datetime.datetime.now().timestamp()
-    start = user.created_at.timestamp()
-    to_send = [(delay, msg) for delay, msg in MESSAGES if now > (delay + start)]
-    if not to_send:
-        return
 
-    history = [m async for m in client.get_chat_history(chat_id, limit=50)]
+    to_send: list[FunnelMessage] = []
+
+    start = user.created_at.timestamp()
+    for v in MESSAGES:
+        if now > (start + v.delay):
+            to_send.append(v)
+        start += v.delay
+
+    if not to_send:
+        return UserStatus.FINISHED
+
+    history: list[Message] = [
+        m async for m in client.get_chat_history(chat_id, limit=50)
+    ]
     history.reverse()
+
     for message in history:
-        message: Message
-        if not message.outgoing:
-            continue
-        # slice to_send by last sent message
-        for v in to_send:
-            delay, msg = v
-            if message.text == msg.strip():
-                to_send = to_send[to_send.index(v) + 1 :]
-                break
-    if to_send:
+        if message.outgoing:
+            # slice to_send by last sent message
+            for i, v in enumerate(to_send):
+                for trigger_phrase in v.triggers + (v.text, "прекрасно", "ожидать"):
+                    if trigger_phrase.lower() in message.text.lower():
+                        if v.trigger_action == "break":
+                            return UserStatus.FINISHED
+                        else:
+                            to_send = to_send[i + 1 :]
+                            break
+    if not to_send:
+        return UserStatus.FINISHED
+    else:
         await client.send_message(chat_id, to_send[0][1])
