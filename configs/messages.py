4b708b80ba652739dd4c2c1f994fa0631395a153
warnaz <124733559+warnaz@users.noreply.github.com>
2024-06-17 17:30:48 +0300

    Update messages.py

--- configs/messages.py
+++ configs/messages.py
@@ -1,6 +1,29 @@
-MESSAGES: tuple[tuple[float | int, str]] = {
-    (6 * 60, "Текст1"),  # 6min
-    (39 * 60, "Текст2"),  # 39min
-    (26 * 60 * 60, "Текст3"),  # 1d 2h
-}
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class FunnelMessage:
+    delay: float | int
+    text: str
+    triggers: tuple[str, ...] = ()
+    trigger_action: Literal["continue", "break"] = "continue"
+
+
+MESSAGES: tuple[FunnelMessage] = (
+    FunnelMessage(
+        delay=6 * 60,
+        text="Текст1",
+    ),
+    FunnelMessage(
+        delay=39 * 60,
+        text="Текст2",
+        triggers=("Триггер1",),
+        trigger_action="continue",
+    ),
+    FunnelMessage(
+        delay=26 * 60 * 60,
+        text="Текст3",
+    ),
+)
 """Format: (delay_seconds, message)"""
